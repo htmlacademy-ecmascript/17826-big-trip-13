6c53856f279e31d837123c1ce1598dcdc8b2616e
Wh_Frost <whfrost63@gmail.com>
2021-01-21 12:55:45 +0400

    Реализует сортировку точек

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -5,20 +5,9 @@ const pointsSortDate = (a, b) => {
 };
 
 const pointsSortDuration = (a, b) => {
-  // const durationA = dayjs(a.endTime).diff(dayjs(a.startTime));
-  // const durationA = dayjs(a.endTime).diff(dayjs(a.startTime, `minute`));
-  // const durationB = dayjs(b.endTime).diff(dayjs(b.startTime));
-  // console.log(durationA);
-  // return;
-
-
-  if (dayjs(a.endTime).diff(a.startTime) < dayjs(b.endTime).diff(b.startTime)) {
-    return 1;
-  }
-  if (dayjs(a.endTime).diff(a.startTime) > dayjs(b.endTime).diff(b.startTime)) {
-    return -1;
-  }
-  return 0;
+  const durationA = dayjs(a.timeEnd).diff(a.timeStart, `minute`);
+  const durationB = dayjs(b.timeEnd).diff(b.timeStart, `minute`);
+  return durationA - durationB;
 };
 
 const pointsSortPrice = (a, b) => {
